0d0f60f8f119b9407c324c2823bcbad44d898914
look001 <45554598+rnner900@users.noreply.github.com>
2026-04-14 19:24:31 +0200

    renaming Anchor3d back to Anchor. hope it doesnt collide https://github.com/google-ai-edge/mediapipe/pull/2530

--- mediapipe_api/graphs/instant_motion_tracking/calculators/transformations.h
+++ mediapipe_api/graphs/instant_motion_tracking/calculators/transformations.h
@@ -13,11 +13,11 @@
 
 extern "C" {
 
-MP_CAPI(MpReturnCode) mp__MakeAnchor3dVectorPacket__PA_i(const mediapipe::Anchor3d* value, int size, mediapipe::Packet** packet_out);
-MP_CAPI(MpReturnCode) mp__MakeAnchor3dVectorPacket_At__PA_i_Rt(const mediapipe::Anchor3d* value, int size, mediapipe::Timestamp* timestamp,
+MP_CAPI(MpReturnCode) mp__MakeAnchor3dVectorPacket__PA_i(const mediapipe::Anchor* value, int size, mediapipe::Packet** packet_out);
+MP_CAPI(MpReturnCode) mp__MakeAnchor3dVectorPacket_At__PA_i_Rt(const mediapipe::Anchor* value, int size, mediapipe::Timestamp* timestamp,
                                                                mediapipe::Packet** packet_out);
-MP_CAPI(MpReturnCode) mp_Packet__GetAnchor3dVector(mediapipe::Packet* packet, mp_api::StructArray<mediapipe::Anchor3d>* value_out);
-MP_CAPI(void) mp_Anchor3dArray__delete(mediapipe::Anchor3d* anchor_vector_data);
+MP_CAPI(MpReturnCode) mp_Packet__GetAnchor3dVector(mediapipe::Packet* packet, mp_api::StructArray<mediapipe::Anchor>* value_out);
+MP_CAPI(void) mp_Anchor3dArray__delete(mediapipe::Anchor* anchor_vector_data);
 
 }  // extern "C"
 
